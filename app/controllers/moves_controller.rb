cf23c2a1ed21d457bfbe942ecd8a3c65e4060731
David Pedersen <david.pdrsn@gmail.com>
2014-10-16 15:57:46 +0200

    Add MovesController and specs

---
+++ app/controllers/moves_controller.rb
@@ -0,0 +1,31 @@
+class MovesController < ApplicationController
+  before_action :require_secret
+
+  rescue_from ActionController::ParameterMissing, ActiveRecord::RecordInvalid do
+    render json: { error: "Move params are missing or invalid" }, status: :unprocessable_entity
+  end
+
+  def index
+    render json: Move.most_popular_names
+  end
+
+  def create
+    @move = Move.new(move_params)
+
+    if @move.save!
+      render json: @move, status: :created
+    end
+  end
+
+  private
+
+  def move_params
+    params.require(:move).permit(:name)
+  end
+
+  def require_secret
+    unless params[:api_key] == ENV["api_key"]
+      render json: { error: "Wrong key" }, status: :unauthorized
+    end
+  end
+end
---
+++ spec/controllers/moves_controller_spec.rb
@@ -0,0 +1,49 @@
+require 'rails_helper'
+
+describe MovesController do
+  describe '#index' do
+    it 'returns the most recent moves' do
+      move = create(:move)
+
+      get :index, format: :json, api_key: ENV["api_key"]
+
+      expect(json_response[0]).to eq(move.name)
+    end
+
+    it 'requires the api key' do
+      get :index, format: :json, api_key: "wrong-key"
+      expect(json_response['error']).to_not be_nil
+      expect(response.status).to eq(401)
+    end
+  end
+
+  describe '#create' do
+    it 'creates new moves' do
+      expect do
+        post :create, format: :json, api_key: ENV["api_key"], move: { name: "a move" }
+      end.to change { Move.count }.by 1
+
+      expect(response.status).to eq(201)
+    end
+
+    it 'does not create the move if its invalid' do
+      expect do
+        post :create, format: :json, api_key: ENV["api_key"], move: { name: '' }
+      end.not_to change { Move.count }
+
+      expect(response.status).to eq(422)
+    end
+
+    it 'does not create the move params are missing' do
+      expect do
+        post :create, format: :json, api_key: ENV["api_key"]
+      end.not_to change { Move.count }
+
+      expect(response.status).to eq(422)
+    end
+  end
+
+  def json_response
+    JSON.parse(response.body)
+  end
+end
