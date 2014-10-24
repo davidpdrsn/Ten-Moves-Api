163bcb40a609845a4d240c86ea8a391163bfbaed
David Pedersen <david.pdrsn@gmail.com>
2014-10-24 12:51:19 +0200

    Refactor TopList

--- app/models/top_list.rb
+++ app/models/top_list.rb
@@ -4,12 +4,26 @@ class TopList
   end
 
   def top(count, method_name)
+    hash_to_array(values_and_their_count(method_name))
+      .sort_by(&:last)
+      .reverse
+      .take(10)
+      .map(&:first)
+  end
+
+  private
+
+  def values_and_their_count(method_name)
     @things.inject(Hash.new(0)) do |acc, thing|
       value = thing.public_send(method_name)
       acc[value] = acc[value] + 1
       acc
-    end.inject([]) do |acc, (value, count)|
+    end
+  end
+
+  def hash_to_array(hash)
+    hash.inject([]) do |acc, (value, count)|
       acc << [value, count]
-    end.sort_by(&:last).reverse.take(10).map(&:first)
+    end
   end
 end
