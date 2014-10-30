bc6d1487bc64544402e9770e55f6c704787aa800
David Pedersen <david.pdrsn@gmail.com>
2014-10-30 22:33:03 +0100

    Refactor TopList#top
    Break long method chain apart into separate shorter methods

--- app/services/top_list.rb
+++ app/services/top_list.rb
@@ -4,21 +4,24 @@ class TopList
   end
 
   def top(count, method_name)
-    values_and_their_count(method_name)
-      .to_a
-      .sort_by(&:last)
-      .reverse
-      .take(10)
-      .map(&:first)
+    responses_to_message(method_name).take(count)
   end
 
   private
+
+  def responses_to_message(method_name)
+    responses_grouped_by_frequency(method_name).map(&:first)
+  end
+
+  def responses_grouped_by_frequency(method_name)
+    values_and_their_count(method_name).sort_by(&:last).reverse
+  end
 
   def values_and_their_count(method_name)
     @things.inject(Hash.new(0)) do |acc, thing|
       value = thing.public_send(method_name)
       acc[value] = acc[value] + 1
       acc
-    end
+    end.to_a
   end
 end
